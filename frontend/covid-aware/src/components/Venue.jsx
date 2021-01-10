0493c4078d04b5b35c34468f485d5e1bf63edcd3
AlexLuo602 <alexluo602@gmail.com>
2021-01-09 22:57:26 -0800

    added basic layout to Venue.jsx

--- frontend/covid-aware/src/components/Venue.jsx
+++ frontend/covid-aware/src/components/Venue.jsx
@@ -2,6 +2,10 @@ import React from "react";
 
 export default function Venue() {
     return <div>
-        <h1>Venue</h1>
+        <h1><b>Is this where you are at?</b></h1>
+        <p><b>Venue ID: </b></p>
+        <p><b>Address: </b></p>
+        <p><b>Email: </b></p>
+        <p><b>Phone number: </b></p>
     </div>;
 }
