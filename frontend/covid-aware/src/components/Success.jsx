e8d5e176e986e651198c558aa981e93cd1ec9b9e
K-zhao <kevzhao2@gmail.com>
2021-01-09 23:06:10 -0800

    Added text to Success.jsx

--- frontend/covid-aware/src/components/Success.jsx
+++ frontend/covid-aware/src/components/Success.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Icon from '@material-ui/core/Icon';
+import CloudDoneRoundedIcon from '@material-ui/icons/CloudDoneRounded';
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 
 export default function Success() {
+    
+
+
+    
     return <div>
-        <h1>Successs</h1>
+        <center><CloudDoneRoundedIcon></CloudDoneRoundedIcon></center>
+        <center><h1>Success!</h1></center>
+        <center><h2>Thanks for checking in!</h2></center>
     </div>;
 }
